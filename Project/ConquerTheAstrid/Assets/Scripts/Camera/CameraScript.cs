6bd7c38de3b6a8b2b911ea335296d1074a112763
nnra6864 <enenragd@gmail.com>
2024-05-09 12:41:21 +0200

    Implemented smooth zoom

--- Project/ConquerTheAstrid/Assets/Scripts/Camera/CameraScript.cs
+++ Project/ConquerTheAstrid/Assets/Scripts/Camera/CameraScript.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using Assets.NnUtils.Scripts;
-using Core;
 using NnUtils.Scripts;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,23 +9,39 @@ namespace Camera
 {
     public class CameraScript : MonoBehaviour
     {
-        private float _currentZoom, _targetZoom;
+        private float _targetZoom;
 
         [SerializeField] private Vector2 _zoomMinMax;
         [SerializeField] private float _zoomSpeed = 0.5f;
+        [SerializeField] private float _zoomLerpTime = 1;
         [SerializeField] private Easings.Types _zoomEasing = Easings.Types.ExpoOut;
 
-        private void Update() => Zoom(1);
+        private void Start() => _targetZoom = transform.localPosition.z; //Update with the current planet instead
+
+        private void Update() => Zoom(InputSystem.actions["Zoom"].ReadValue<float>());
 
         private void Zoom(float amount)
         {
-            Debug.Log(amount);
+            if (amount == 0) return;
+            _targetZoom = Mathf.Clamp(_targetZoom + (amount * _zoomSpeed), _zoomMinMax.x, _zoomMinMax.y);
+            Misc.RestartCoroutine(this, ref _lerpZoomRoutine, LerpZoomRoutine());
         }
         
         private Coroutine _lerpZoomRoutine;
         private IEnumerator LerpZoomRoutine()
         {
-            yield return null;
+            var startZoom = transform.localPosition.z;
+            float lerpPos = 0;
+
+            while (lerpPos < 1)
+            {
+                var t = Misc.UpdateLerpPos(ref lerpPos, _zoomLerpTime, false, _zoomEasing);
+                var zoom = Mathf.Lerp(startZoom, _targetZoom, t);
+                transform.localPosition = new (transform.localPosition.x, transform.localPosition.y, zoom);
+                yield return null;
+            }
+            
+            _lerpZoomRoutine = null;
         }
 
         private void SetMinMax()
